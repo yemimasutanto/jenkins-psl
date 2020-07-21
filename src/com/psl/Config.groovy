6dad343128de2fdda596630394cbd836b569d83c
Denny Setiawan <denny.setiawan@tokopedia.com>
2020-07-21 16:50:42 +0700

    build stage

--- src/com/psl/Config.groovy
+++ src/com/psl/Config.groovy
@@ -6,7 +6,6 @@ class Config {
     def default_docker_jenkins_tool = 'docker'
     def default_docker_registry = "https://registry-1.docker.io/v2/"
     def default_docker_registry_jenkins_cred = 'docker-credentials'
-    def default_docker_jenkins_tool = 'docker'
 
     // Golang related
     def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
